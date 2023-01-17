539b4bdde00552b017d35a92d08819df717f42b9
Mehmet Mert Bese <mehmetmert.bese@gmail.com>
2023-01-17 22:35:07 +0100

    cc_ex3-configjs

---
+++ cc_ex3/server/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    'server': {
+        'port' : 3000,
+        'host': 'localhost'
+    },
+    'hello-world-service': {
+        'port' : 9001,
+        'host': 'hello-world-service'
+    },
+    'product-descp-service': {
+        'port' : 9002,
+        'host': 'product-descp-service'
+    },
+    'product-price-service': {
+        'port' : 3000,
+        'host': 'product-price-service'
+    },
+
+}
